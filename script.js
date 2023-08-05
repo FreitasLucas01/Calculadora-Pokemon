7639ceb8d93282c93bf4baf969e902703cc53328
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2023-08-04 22:11:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const pokemon = document.querySelector(".pokemon-resultado");
 const pokemonImg = document.querySelector(".pokemon-img");
 const pokemonNumero = document.querySelector(".pokemon-numero");
 
-const capturarCliques = () => {
-  document.addEventListener("click", (e) => {
-    const el = e.target;
-    el.classList.contains("btn-display") ? addNumDisplay(el) : false;
-    el.classList.contains("btn-zerar") ? clear() : false;
-    el.classList.contains("btn-apagar") ? del() : false;
-    el.classList.contains("btn-resultado") ? realizaConta() : false;
-  });
-};
+function handleButtons(e) {
+  if (e.type === "touchstart") e.preventDefault();
+  const el = e.target;
+  el.classList.contains("btn-display") ? addNumDisplay(el) : false;
+  el.classList.contains("btn-zerar") ? clear() : false;
+  el.classList.contains("btn-apagar") ? del() : false;
+  el.classList.contains("btn-resultado") ? realizaConta() : false;
+}
 
 async function pokemonNome(conta) {
   if (Number.isInteger(conta)) {
@@ -68,71 +67,5 @@ const realizaConta = () => {
   }
 };
 
-if (window.matchMedia("(max-width: 600px)").matches) {
-  const capturarCliques = () => {
-    document.addEventListener("click", (e) => {
-      const el = e.target;
-      el.classList.contains("btn-display") ? addNumDisplay(el) : false;
-      el.classList.contains("btn-zerar") ? clear() : false;
-      el.classList.contains("btn-apagar") ? del() : false;
-      el.classList.contains("btn-resultado") ? realizaConta() : false;
-    });
-  };
-
-  async function pokemonNome(conta) {
-    if (Number.isInteger(conta)) {
-      const pokeFetch = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${conta}/`
-      );
-      const pokeAPI = await pokeFetch.json();
-      const pokemonName = pokeAPI.forms[0].name;
-
-      const capitalize = (s) =>
-        typeof s !== "string" ? "" : s.charAt(0).toUpperCase() + s.slice(1);
-
-      pokemon.classList.add("ativo");
-      pokemonImg.src = pokeAPI.sprites.front_default;
-      pokemonNumero.innerText = `Nº ${conta} - ${capitalize(pokemonName)}`;
-    } else {
-      return false;
-    }
-  }
-
-  const removeAtivo = () => {
-    pokemon.classList.remove("ativo");
-  };
-
-  const clear = () => {
-    display.value = "";
-    removeAtivo();
-  };
-
-  const del = () => {
-    display.value = display.value.slice(0, -1);
-    removeAtivo();
-  };
-
-  const addNumDisplay = (el) => {
-    display.value += el.innerText;
-    display.focus();
-  };
-
-  const realizaConta = () => {
-    try {
-      const conta = eval(display.value);
-
-      if (!conta) {
-        alert("Conta inválida");
-        return;
-      } else {
-        display.value = conta;
-        pokemonNome(conta);
-      }
-    } catch {
-      alert("Conta inválida");
-      return;
-    }
-  };
-}
-
-capturarCliques();
+document.addEventListener("click", handleButtons);
+document.addEventListener("touchstart", handleButtons);
